54a9cbff4ea0d7337bb39dff700a0f6cf7f89dc1
Hayley K <mrs.kornilenko@gmail.com>
2018-07-27 15:34:24 -0400

    fixed intialstate function

--- onclick.js
+++ onclick.js
@@ -1,21 +1,21 @@
 var firstNumber = 0;
 var secondNumber = 0;
-var operator = "";
+var operator = '';
 var result = 0;
 var operatorChoosen = false;
 
 // FUNCTIONS
 
 function initialState() {
-  var firstNumber = 0;
-  var secondNumber = 0;
-  var operator = "";
-  var result = 0;
-  var operatorChoosen = false;
-  $("#first-number").empty();
-  $("#operator").empty();
-  $("#second-number").empty();
-  $("#result").empty();
+  firstNumber = 0;
+  secondNumber = 0;
+  operator = '';
+  result = 0;
+  operatorChoosen = false;
+  $('#first-number').empty();
+  $('#operator').empty();
+  $('#second-number').empty();
+  $('#result').empty();
 }
 
 function add(x, y) {
@@ -40,56 +40,56 @@ function powerOf(x, y) {
 
 // onClick
 
-$(".number").click(function() {
+$('.number').click(function() {
   var buttonClicked = this;
   if (operatorChoosen == false) {
     buttonValue = buttonClicked.value;
-    $("#first-number").text(buttonValue);
+    $('#first-number').text(buttonValue);
     operatorChoosen = true;
     firstNumber = buttonValue;
   } else {
     buttonValue2 = buttonClicked.value;
-    $("#second-number").text(buttonValue2);
+    $('#second-number').text(buttonValue2);
     secondNumber = buttonValue2;
   }
 });
 
-$(".operator").click(function() {
+$('.operator').click(function() {
   var buttonClicked = this;
-  if (buttonClicked.value == "plus") {
-    $("#operator").text("+");
-    operator = "+";
-  } else if (buttonClicked.value == "minus") {
-    $("#operator").text("-");
-    operator = "-";
-  } else if (buttonClicked.value == "times") {
-    $("#operator").text("x");
-    operator = "x";
-  } else if (buttonClicked.value == "divide") {
-    $("#operator").text("/");
-    operator = "/";
-  } else if (buttonClicked.value == "power") {
-    $("#operator").text("^");
-    operator = "^";
+  if (buttonClicked.value == 'plus') {
+    $('#operator').text('+');
+    operator = '+';
+  } else if (buttonClicked.value == 'minus') {
+    $('#operator').text('-');
+    operator = '-';
+  } else if (buttonClicked.value == 'times') {
+    $('#operator').text('x');
+    operator = 'x';
+  } else if (buttonClicked.value == 'divide') {
+    $('#operator').text('/');
+    operator = '/';
+  } else if (buttonClicked.value == 'power') {
+    $('#operator').text('^');
+    operator = '^';
   }
 });
 
-$(".equal").click(function() {
+$('.equal').click(function() {
   var buttonClicked = this;
-  $("#result").text("=");
-  if (operator == "+") {
-    $("#result").text("=" + add(firstNumber, secondNumber));
-  } else if (operator == "-") {
-    $("#result").text("=" + subtract(firstNumber, secondNumber));
-  } else if (operator == "x") {
-    $("#result").text("=" + multiply(firstNumber, secondNumber));
-  } else if (operator == "/") {
-    $("#result").text("=" + divide(firstNumber, secondNumber));
-  } else if (operator == "^") {
-    $("#result").text("=" + powerOf(firstNumber, secondNumber));
+  $('#result').text('=');
+  if (operator == '+') {
+    $('#result').text('=' + add(firstNumber, secondNumber));
+  } else if (operator == '-') {
+    $('#result').text('=' + subtract(firstNumber, secondNumber));
+  } else if (operator == 'x') {
+    $('#result').text('=' + multiply(firstNumber, secondNumber));
+  } else if (operator == '/') {
+    $('#result').text('=' + divide(firstNumber, secondNumber));
+  } else if (operator == '^') {
+    $('#result').text('=' + powerOf(firstNumber, secondNumber));
   }
 });
 
-$(".clear").click(function() {
+$('.clear').click(function() {
   initialState();
 });
